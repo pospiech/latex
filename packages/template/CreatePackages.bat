d8729fd31e52db0ed62deec4a0ef15e419e1a48d
m.pospiech79 <m.pospiech79@gmail.com>
2012-07-17 20:29:24 +0000

    Added file to generate packages for CTAN

---
+++ packages/template/CreatePackages.bat
@@ -0,0 +1,93 @@
+@ECHO OFF
+cls
+
+SET TEXENGINE=pdflatex
+SET TARGETDIR=CTAN
+call:funcCreateDir %TARGETDIR%
+
+:: Package: templatedemo
+SET PACKAGE=templatedemo
+SET FOLDER=%TARGETDIR%\%PACKAGE%
+call:createPackage %FOLDER% %PACKAGE%
+
+:: Package: templatesection
+SET PACKAGE=templatesection
+SET FOLDER=%TARGETDIR%\%PACKAGE%
+call:createPackage %FOLDER% %PACKAGE%
+
+:: Package: templatetools
+SET PACKAGE=templatetools
+SET FOLDER=%TARGETDIR%\%PACKAGE%
+call:createPackage %FOLDER% %PACKAGE%
+
+call:CleanUP
+
+echo.&pause&goto:eof
+
+
+
+::--------------------------------------------------------
+::-- Function section starts below here
+::--------------------------------------------------------
+
+:createPackage
+	set FOLDER=%~1
+	set PACKAGE=%~2
+	call:funcCreateDir %FOLDER%
+	copy %PACKAGE%.ins %FOLDER%
+	ExchangeInputByFile.pl %PACKAGE%.dtx "%FOLDER%\%PACKAGE%.dtx"
+	REM Compile
+	set OLDDIR=%CD%
+	cd "%FOLDER%"
+	call:Compile %PACKAGE%
+	REM cleanup
+	call:CleanUP
+	chdir /d %OLDDIR%
+	goto:eof
+
+:funcCreateDir
+	echo "creating folder %~1"
+	IF NOT EXIST "%~1" md "%~1"
+	goto:eof
+
+:Compile	
+	SET TEXFILE=%~1
+	echo "creating .sty and .pdf file of package %TEXFILE%"
+	call %TEXENGINE% -shell-escape -interaction=nonstopmode %TEXFILE%.ins
+	call %TEXENGINE% -shell-escape -interaction=nonstopmode %TEXFILE%.dtx
+	call %TEXENGINE% -shell-escape -interaction=nonstopmode %TEXFILE%.dtx
+	goto:eof
+
+:: clean up
+:CleanUP
+@del *.aux
+@del *.dvi
+@del *.acn
+@del *acr
+@del *alg
+@del *blg
+@del *glg
+@del *gls
+@del *slg
+@del *syi
+@del *blx.bib
+@del *.glo
+@del *.idx
+@del *.ilg
+@del *.ind
+@del *.ist
+@del *.lof
+@del *.lol
+@del *.lot
+@del *.ps
+@del *.gnuplot
+@del *.table
+@del *.xml
+@del *.syg
+@del *.toc
+@del *.bcf
+@del democode.tex
+@del *.log
+goto:eof
+
+
