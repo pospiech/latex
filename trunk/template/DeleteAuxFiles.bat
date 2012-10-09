db106c1d99240de88263691ac61f10ea68a74c64
m.pospiech79@gmail.com <m.pospiech79@gmail.com@d6f95a57-76a5-fac6-b003-34666e3314b4>
2012-10-09 19:01:57 +0000

    Changed error in UseDefinition to warning Started text in 0-Introduction.tex Added further conditionals in demo.tex if xcolor is not loaded Started 'Known problems' chapter in documentation.tex Tried cross-refs, but so far they do not work, removed 'sortlos=los' in style-biblatex.tex, added color conditional in style-hyperref.tex, further conditionals in style.tex,

---
+++ trunk/template/DeleteAuxFiles.bat
@@ -0,0 +1,48 @@
+@ECHO OFF
+cls
+
+call:CleanUP
+
+echo.&pause&goto:eof
+
+::--------------------------------------------------------
+::-- Function section starts below here
+::--------------------------------------------------------
+
+:: clean up
+:CleanUP
+del *.aux
+del *.dvi
+del *.acn
+del *acr
+del *alg
+del *blg
+del *glg
+del *gls
+del *slg
+del *syi
+del *blx.bib
+del *.glo
+del *.idx
+del *.ilg
+del *.ind
+del *.ist
+del *.lof
+del *.lol
+del *.lot
+del *.ps
+del *.gnuplot
+del *.table
+del *.xml
+del *.syg
+del *.toc
+del *.bcf
+del *.tdo
+del *.bak
+del *.filelist
+del *.gz
+del fit.log
+del plotdata.txt
+
+
+
