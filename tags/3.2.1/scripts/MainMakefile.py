1592dd733d3e0b053324d957e853e330853b86bb
m.pospiech79@gmail.com <m.pospiech79@gmail.com@d6f95a57-76a5-fac6-b003-34666e3314b4>
2014-04-15 18:37:37 +0000

    Release 3.2.1 (Version 2 with bugfix 1)

---
+++ tags/3.2.1/scripts/MainMakefile.py
@@ -0,0 +1,126 @@
+#-------------------------------------------------------------------------------
+# Name:        createDocumentation
+# Purpose:
+#
+# Author:      Matthias Pospiech
+#
+# Created:     01.04.2013
+# Copyright:   (c) Matthias Pospiech 2013
+# Licence:     GPL
+#-------------------------------------------------------------------------------
+
+import os
+import shutil
+import datetime
+import texBuildFunctions as tex
+import insertPrintCode as importInsertPrintCode
+import fontexamples as importFontExamples
+import createPackages as importCreatePackages
+
+def main():
+    print ("--- clean up main folder before compilation ---")
+    tex.unfailingRemoveFile('../template/TemplateDocumentation-figure*.log')
+    tex.cleanupRecursiveAuxFiles('../template/', '*.aux')
+    texfile = '../template/LaTeXTemplate.'
+    tex.cleanupAuxFiles(texfile)
+    texfile = '../template/TemplateDocumentation.'
+    tex.cleanupAuxFiles(texfile)
+
+    print ("--- Creation of Packages ---")
+    importCreatePackages.main()
+
+    print ("--- Copy Packages ---")
+    copyPackages()
+
+    print ("--- Create Font Examaples ---")
+    importFontExamples.main()
+
+    print ("--- fill doc-code.tex ---")
+    importInsertPrintCode.main()
+
+    print ("--- compiling LaTeXTemplate.tex ---")
+    texfile = '../template/LaTeXTemplate.tex'
+    tex.compileLatexDocument(texfile)
+    copyTeXFile(texfile)
+
+    print ("--- compiling TemplateDocumentation.tex ---")
+    texfile = '../template/TemplateDocumentation.tex'
+    tex.compileLatexDocument(texfile)
+    copyFile(texfile.replace('.tex', '.pdf'), '')
+    tex.cleanupRecursiveAuxFiles('../template/', '*.aux')
+    tex.unfailingRemoveFile('../template/content/demo/democode.tex')
+    tex.unfailingRemoveFile('../template/content/longtable.tex')
+    tex.unfailingRemoveFile('../template/demo-glossaries-*')
+
+
+
+    copyFile('bib/BibtexDatabase.bib', 'bib')
+    copyFile('content/demo/latextutorial.tex', 'content/demo/')
+    copyFile('content/*.tex', 'content')
+    copyFile('fonts/*.tex', 'fonts')
+    copyFile('images/*', 'images')
+    copyFile('macros/*', 'macros')
+    copyFile('preamble/commands.tex', 'preamble')
+    copyFile('preamble/makeCommands.tex', 'preamble')
+    copyFile('preamble/fix*.tex', 'preamble')
+    copyFile('preamble/listings*.tex', 'preamble')
+    copyFile('preamble/packages*.tex', 'preamble')
+    copyFile('preamble/style*.tex', 'preamble')
+    copyFile('version.txt', '')
+    copyFile('licence.txt', '')
+    copyFile('*.sty', '')
+
+
+    # zip publish folder
+
+    oldPath = os.getcwd()
+    os.chdir('../template/')
+
+    currentDateStr = datetime.datetime.now().strftime("%Y-%m-%d")
+    filename = 'latexthesistemplate-' + currentDateStr + '.zip'
+    executeCode = 'zip -r ' +  filename + ' latexthesistemplate/*'
+    print (executeCode)
+    result = os.system(executeCode)
+
+    os.chdir(oldPath)
+
+
+def copyFile(filename, targetFolder):
+    strBaseDir = '../template/latexthesistemplate/'
+    tex.ensureDirectoryExists(strBaseDir)
+    strTargetDir = strBaseDir + targetFolder
+    tex.ensureDirectoryExists(strTargetDir)
+    strFileOrigin = '../template/' + filename
+    tex.copyfiles(strFileOrigin, strTargetDir)
+
+
+def copyTeXFile(texfile):
+    strTargetDir = '../template/latexthesistemplate'
+    tex.ensureDirectoryExists(strTargetDir)
+    shutil.copy(texfile, strTargetDir)
+    shutil.copy(texfile.replace('.tex', '.pdf'), strTargetDir)
+
+
+
+
+def copyPackage(package):
+    packagesPath = r'..\packages\publish\CTAN'
+    templatePath = r'..\template'
+    packagefile = packagesPath + "\\" + package + "\\" + package + ".sty"
+    if os.path.exists(packagefile):
+        shutil.copy(packagefile, templatePath)
+        print (package)
+    else:
+        print (package + ' not found')
+
+
+def copyPackages():
+    copyPackage('doctools')
+    copyPackage('lastpackage')
+    copyPackage('tablestyles')
+    copyPackage('latexdemo')
+    copyPackage('codesection')
+    copyPackage('templatetools')
+
+if __name__ == '__main__':
+    main()
---
+++ tags/3.2.1/scripts/texBuildFunctions.py
@@ -0,0 +1,191 @@
+import os
+import glob
+import fnmatch
+import subprocess
+import sys
+import glob
+import shutil
+import re
+
+def main():
+    logfile = '../template/TemplateDocumentation.log'
+    outputFile = '../template/TemplateDocumentation-FileList.txt'
+    extractFileList(logfile, outputFile)
+
+def extractFileList(logfile, outputFile):
+    strStart = ' *File List*'
+    strEnd = ' ***********'
+    logFileListValid = False
+    array = []
+
+    with open(logfile, 'r') as parseFileHandle:
+        with open(outputFile, 'w') as outputFileHandle:
+            for lineParse in parseFileHandle:
+                # test for string '%%?'
+                if lineParse.startswith(strEnd):
+                    logFileListValid = False
+                    break
+
+                if lineParse.startswith(strStart):
+                    logFileListValid = True
+                    continue
+
+                if (logFileListValid):
+                    array.append( lineParse )
+                    logFileEntryNumbers = 0
+                    for lineInput in array:
+                        if lineInput == lineParse:
+                            logFileEntryNumbers = logFileEntryNumbers + 1
+                            # print (lineInput)
+                        if logFileEntryNumbers > 2:
+                            # print(lineParse)
+                            break
+                    # output only if entry is unique
+                    if logFileEntryNumbers < 2:
+                        # write line to output
+                        outputFileHandle.write(lineParse)
+
+def ensureDirectoryExists(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def unfailingRemoveFile(filename):
+    for filePath in glob.glob(filename):
+        print(filePath)
+        if os.path.isfile(filePath):
+            os.remove(filePath)
+            print("removed: " + filePath)
+        else:
+            print(filePath + " not found")
+
+def cleanupRecursiveAuxFiles(rootPath, filetype):
+    for root, dirs, files in os.walk(rootPath):
+        for filename in fnmatch.filter(files, filetype):
+            unfailingRemoveFile(os.path.join(root, filename))
+
+# from http://stackoverflow.com/questions/2584414/python-windows-file-copy-with-wildcard-support
+def copyfiles(src_glob, dst_folder):
+    for fname in glob.iglob(src_glob):
+        filename = fname.replace('\\', '/')
+        print(filename )
+        shutil.copy(filename, dst_folder)
+
+def cleanupAuxFiles(filename):
+    unfailingRemoveFile(filename + 'aux')
+    unfailingRemoveFile(filename + 'aux')
+    unfailingRemoveFile(filename + 'dvi')
+    unfailingRemoveFile(filename + 'acn')
+    unfailingRemoveFile(filename + 'acr')
+    unfailingRemoveFile(filename + 'alg')
+    unfailingRemoveFile(filename + 'blg')
+    unfailingRemoveFile(filename + 'glg')
+    unfailingRemoveFile(filename + 'gls')
+    unfailingRemoveFile(filename + 'slg')
+    unfailingRemoveFile(filename + 'syi')
+    unfailingRemoveFile(filename + 'blx.bib')
+    unfailingRemoveFile(filename + 'glo')
+    unfailingRemoveFile(filename + 'idx')
+    unfailingRemoveFile(filename + 'ilg')
+    unfailingRemoveFile(filename + 'ind')
+    unfailingRemoveFile(filename + 'ist')
+    unfailingRemoveFile(filename + 'lof')
+    unfailingRemoveFile(filename + 'lol')
+    unfailingRemoveFile(filename + 'lot')
+    unfailingRemoveFile(filename + 'ps')
+    unfailingRemoveFile(filename + 'gnuplot')
+    unfailingRemoveFile(filename + 'table')
+    unfailingRemoveFile(filename + 'xml')
+    unfailingRemoveFile(filename + 'syg')
+    unfailingRemoveFile(filename + 'toc')
+    unfailingRemoveFile(filename + 'bcf')
+    unfailingRemoveFile(filename + 'tdo')
+    unfailingRemoveFile(filename + 'bak')
+    unfailingRemoveFile(filename + 'filelist')
+    unfailingRemoveFile(filename + 'gz')
+    unfailingRemoveFile(filename + 'run.xml')
+    unfailingRemoveFile(filename + 'out')
+    unfailingRemoveFile(filename + 'hd')
+    unfailingRemoveFile(filename + 'ptc')
+    unfailingRemoveFile(filename + 'mw')
+    unfailingRemoveFile(filename + 'bbl')
+    unfailingRemoveFile(filename + 'pgf-plot.gnuplot')
+    unfailingRemoveFile(filename + 'pgf-plot.table')
+    unfailingRemoveFile(filename + 'plotdata.gnuplot')
+    unfailingRemoveFile(filename + 'plotdata.table')
+    unfailingRemoveFile(filename + 'tex.blg')
+    unfailingRemoveFile(filename + 'wrt')
+    unfailingRemoveFile(filename + 'auxlock')
+    unfailingRemoveFile(filename + 'glsdefs')
+    unfailingRemoveFile(filename + 'synctex.gz')
+    unfailingRemoveFile('plotdata.txt')
+    unfailingRemoveFile('fit.log')
+    unfailingRemoveFile('*FileList.txt')
+    # unfailingRemoveFile(filename + 'log')
+
+
+
+# executes python script
+def execfile(filename):
+    exec(compile(open(filename).read(), filename, 'exec'))
+
+
+def callSystemCommand(command):
+    try:
+        print ( command )
+        retcode = subprocess.call(command, shell=True)
+        if retcode != 0:
+            print("System command was terminated by signal", -retcode, file=sys.stderr)
+            print(os.getcwd())
+            sys.exit()
+    except OSError as e:
+        print("Execution failed:", e, file=sys.stderr)
+
+def compileLatexDocument(texfile):
+    # move to path of tex file
+    newPath, filename = os.path.split(texfile)
+    oldPath = os.getcwd()
+    if os.path.exists(newPath):
+        os.chdir(newPath)
+    # base filename
+    texName = filename.replace('.tex', '')
+
+    # delete previous aux files
+    print ('--- delete previous aux files ---' )
+    cleanupRecursiveAuxFiles(newPath, '*.aux')
+
+    print ('--- creating document ' + texName + ' ---' )
+    # call pdflatex
+    callSystemCommand(['pdflatex', '--interaction', 'nonstopmode', '-shell-escape', texName + '.tex'])
+
+    # call makeglossaries
+    callSystemCommand(['makeglossaries', texName])
+
+    # call biber
+    callSystemCommand(['biber', texName])
+
+    # call makeindex
+    callSystemCommand(['makeindex', texName + '.ist'])
+    # executeCode = 'makeindex "' + texName + '.ist"'
+    # result = os.system(executeCode)
+
+    # call pdflatex
+    callSystemCommand(['pdflatex', '--interaction', 'nonstopmode', '-shell-escape', texName + '.tex'])
+
+    logfile = texName + '.log'
+    outputFile = texName + '-FileList' + '.txt'
+    extractFileList(logfile, outputFile)
+
+    # call pdflatex
+    callSystemCommand(['pdflatex', '--interaction', 'nonstopmode', '-shell-escape', texName + '.tex'])
+
+    # call pdflatex
+    callSystemCommand(['pdflatex', '--interaction', 'nonstopmode', '-shell-escape', texName + '.tex'])
+
+    # clean up temp files
+    print ('cleaning up temp files')
+    cleanupAuxFiles(texName + '.')
+    cleanupRecursiveAuxFiles(newPath, '*.aux')
+    # move back to original path
+    os.chdir(oldPath)
+
+# main()
